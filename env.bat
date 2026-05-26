14df256f11e99611c7c7ffb9a821c2f36ada0a42
Ivan Pedruzzi <ivanpedruzzi@hotmail.com>
2026-05-26 14:44:35 -0400

    Add XSL course slide decks
    Ten-slide PowerPoint overviews for XML, XSL-FO, XPath 2.0 and XSLT 2.0, styled after the Stylus Studio website.

---
+++ env.bat
@@ -0,0 +1,5 @@
+SET THIRDPARTY=f:\builds\thirdparties
+SET NODEJS_HOME=%THIRDPARTY%\nodejs\node-v24.13.1-win-x64
+SET JAVA_HOME=%CURRENT_FOLDER%\lib\java\windows\jdk
+SET PATH=%NODEJS_HOME%;%PATH%
+
